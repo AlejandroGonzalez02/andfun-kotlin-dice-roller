0e0fc289e6f770ae12e07b66d161a6f3e65a2c93
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-09-30 13:50:36 +0200

    Exercicio 2 rematado.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -17,13 +17,22 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{
+            Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+        }
+
 
         // TODO (02) Use findViewById to get a reference to the button
         // and assign it to an immutable variable called rollButton
